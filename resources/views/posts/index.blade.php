248afce9f3db84fd5aa89199d11d421bb1f49fe3
M GH <aali361@gmail.com>
2018-08-14 19:31:42 -0400

    Minor bug fixing

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,14 +3,14 @@
 @section('head')
 <style>
         @media screen and (min-width: 480px) {
-            .index-body{
+            .post-body{
                 padding:0px 15%;
                 font-size: 1.4rem;
                 font-family: Georgia,Times,Times New Roman,serif; 
             }
         }
         @media screen and (min-width: 0px) and (max-width:480px) {
-            .index-body{
+            .post-body{
                 padding:0px 2%;
                 font-family: Georgia,Times,Times New Roman,serif; 
                 font-size: 1.2rem;
@@ -20,12 +20,10 @@
 @endsection
 
 @section('content')
-<div class="index-body">
     @foreach($posts as $post)
         @include('posts.post')
         @if(!$loop->last)
             <div style="height: 50px;"></div>
         @endif
     @endforeach
-</div>
 @endsection
--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -1,6 +1,6 @@
 <div class="my-5">
     <a class="display-4 mb-5" style="text-decoration: none;" href="/posts/{{$post->link}}">{{$post->title}}</a>    
-    <div class="my-5 px-1">
+    <div class="my-5 post-body">
         {!!$post->body!!}
     </div>
     <hr class="mb-0">
