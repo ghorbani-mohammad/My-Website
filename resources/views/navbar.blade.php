4bbdeed9345b3df736b118d4a047ed5909806c7f
Mohammad <mohammad.ghorbani@hotmail.com>
2022-11-24 19:52:22 +0330

    minor change

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -19,12 +19,14 @@
                 <li id="about" class="nav-item">
                     <a class="nav-link" href="/contact">Contact Me</a>
                 </li>
+                <li>
+                    <a class="nav-link" href="/contact">Download CV</a>
+                </li>
             </ul>
             <form action="/search" method="post" class="form-inline my-2 my-lg-0">
                 @csrf
                 <input name="keywords" class="form-control mr-sm-2" type="search" placeholder="like: Python or Django" aria-label="Search">
                 <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
             </form>
-            <button class="btn btn-outline-success my-2 my-sm-0">Download CV</button>
         </div>
     </nav>
