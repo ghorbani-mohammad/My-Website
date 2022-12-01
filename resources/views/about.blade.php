cdb23989444d37609c741c2516915693d0a1a887
Mohammad <mohammad.ghorbani@hotmail.com>
2022-12-01 22:11:09 +0330

    minor change

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -23,7 +23,7 @@
             <p class="figure-caption">My name is Mohammad Ghorbani. I was born in a small city called Tafresh. My birth date is 24 April 1994. I have just one brother so we are small family but Lovely one :). I studied Computer Enginnering (CE) at Arak University. I keen to learn new things in computer science and electronic fields. My favorite sports are swimming and hiking. I wish i could have positive impact on my country as an indiviual.</p>
             <p class="figure-caption">I created this website to register my toughts and my projects. It has some dark sides but it has also some benefits as so. The important thing that convinced me to start this website is that it can help me to find peopole similar to myself so i will not waste my time with wrong person (in similariy prespect).</p>
             <br>
-            <p class="lead-5 text-center">This website will help me to find more peopole like myself.</p>
+            <p class="lead-5 text-center">This website will help me to find more people like myself.</p>
             <br>
             <p class="figure-caption">The most important reason that i like to mention is that I will share my projects in past, current and even in future on this website. So as a resume this website will help me to demonstrate my skills to recruiter.</p>
             <br>
