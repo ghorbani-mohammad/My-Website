6c8f089fc0a53b1d74cdafc3e98d18f17f3c7e71
Mohammad <mohammad.ghorbani@hotmail.com>
2025-07-30 13:58:43 +0330

    chore(projects): update project description to direct users to GitHub account

--- resources/views/posts/projects.blade.php
+++ resources/views/posts/projects.blade.php
@@ -9,7 +9,7 @@
 
 @section('content')
     <div class="text-center">
-        <div class="alert alert-info my-5 d-inline-block">Here I will speak about my projects that I have done in past or working on it currently.</div>
+    <div class="alert alert-info my-5">For seeing the total list of my projects, please check my <a href="https://github.com/ghorbani-mohammad" target="_blank" class="alert-link">GitHub account</a>.</div>
     </div>
     @foreach($posts as $post)
     @include('posts.post')
